5e93594d6c2a7835d9ff3a2592934859b7480dbc
Addison Severnak <addisonsevernak@gmail.com>
2022-11-15 21:19:39 -0600

    completed topic 4 and sprint challenge

--- index.js
+++ index.js
@@ -242,8 +242,24 @@ const cuboid = new CuboidMaker({
 //Using CuboidMakerTwo, take your prototypes from above and refactor into class syntax. Then, create an object called cuboidTwo that uses the new keyword to use our CuboidMakerTwo class.
  
 class CuboidMakerTwo{
-
+  constructor({length, width, height}) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
 }
+
+const cuboidTwo = new CuboidMakerTwo({
+  length: 4,
+  width: 5,
+  height: 5,
+});
 
 
 
